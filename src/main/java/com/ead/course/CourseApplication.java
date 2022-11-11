3ecc41cf5a46c689efed7fa5b84235e36cb137f0
DESKTOP-U4HJ6BO\Dell <adalburq@gmail.com>
2022-11-10 23:08:33 -0300

    SEMANA 3

--- src/main/java/com/ead/course/CourseApplication.java
+++ src/main/java/com/ead/course/CourseApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CourseApplication {
 
-	// SEMANA 2
     public static void main(String[] args) {
         SpringApplication.run(CourseApplication.class, args);
     }
