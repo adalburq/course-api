c3aca3da96fb689c4af72fdfdd0df1664d59d58b
DESKTOP-U4HJ6BO\Dell <adalburq@gmail.com>
2022-12-08 10:04:37 -0300

    ajustes de texto

--- src/main/java/com/ead/course/clients/AuthUserClient.java
+++ src/main/java/com/ead/course/clients/AuthUserClient.java
@@ -34,16 +34,20 @@ public class AuthUserClient {
     String REQUEST_URL_AUTHUSER;
 
     public Page<UserDto> getAllUsersByCourse(UUID courseId, Pageable pageable){
+    	
         List<UserDto> searchResult = null;
         ResponseEntity<ResponsePageDto<UserDto>> result = null;
         String url = REQUEST_URL_AUTHUSER + utilsService.createUrlGetAllUsersByCourse(courseId, pageable);
         log.debug("Request URL: {} ", url);
         log.info("Request URL: {} ", url);
+        
         try{
-            ParameterizedTypeReference<ResponsePageDto<UserDto>> responseType = new ParameterizedTypeReference<ResponsePageDto<UserDto>>() {};
+        
+        	ParameterizedTypeReference<ResponsePageDto<UserDto>> responseType = new ParameterizedTypeReference<ResponsePageDto<UserDto>>() {};
             result = restTemplate.exchange(url, HttpMethod.GET, null, responseType);
             searchResult = result.getBody().getContent();
             log.debug("Response Number of Elements: {} ", searchResult.size());
+            
         } catch (HttpStatusCodeException e){
             log.error("Error request /courses {} ", e);
         }
